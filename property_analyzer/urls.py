e493284b5a807d3326c98289de7dd3e93986c84d
annchenruth <aknodt@gmail.com>
2018-11-15 08:46:35 -0500

    updated urls.py

--- property_analyzer/urls.py
+++ property_analyzer/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from property_analyzer.forms import LoginForm
+from django.contrib.auth.views import login, logout
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'),name="home"),
     path('admin/', admin.site.urls),
+    path('rental/', include('rental.urls')),
+    path('iprestrict/', include('iprestrict.urls', namespace='iprestrict')),
+    path('login/', login, {'template_name': 'registration/login.html', 'authentication_form': LoginForm}, name='login'), # not sure why i had to remove name='login' here???
+    path('logout/', logout, name='logout'),      
 ]
